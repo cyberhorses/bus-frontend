0b182d2ae842c117845d55ac906dd1b5a18d862f
sucholbluk <sucholbiaklukasz@gmail.com>
2026-01-11 19:55:47 +0100

    fix: add bus-app to allowed hosts

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [reactRouter(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: ['localhost', '127.0.0.1', 'bus-app.pl']
+  }
 });
