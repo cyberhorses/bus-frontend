ee5358b3e6e7eee59a4f2d48cc3a6324e4ba4c13
sucholbluk <sucholbiaklukasz@gmail.com>
2026-01-12 20:38:20 +0100

    fix: potential refresh fix

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ export default defineConfig({
   plugins: [reactRouter(), tailwindcss()],
   server: {
     host: '0.0.0.0',
-    allowedHosts: ['localhost', '127.0.0.1', 'bus-app.pl']
+    allowedHosts: ['localhost', '127.0.0.1', 'bus-app.pl'],
+    hmr: false,
+    watch: null
   }
 });
